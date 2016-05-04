04fb18e24e73c3fd03b3d20202addeee994bd03c
bckfnn <bckfnn@gmail.com>
2016-05-04 15:28:15 +0200

    javadocs.

--- src/main/java/io/github/bckfnn/callback/Callback.java
+++ src/main/java/io/github/bckfnn/callback/Callback.java
@@ -23,9 +23,43 @@ import java.util.function.Consumer;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 
+/**
+ * A funtional interface that can signal a success value or a failure condition.
+ *
+ * @param <T> type of the success value.
+ */
 public interface Callback<T> {
+    /**
+     * Called when the callback occur.
+     * @param result success value, when error is null
+     * @param error when not null, this is the error condition.
+     */
     public void call(T result, Throwable error);
 
+    /**
+     * Return a new callback where the handler is invoked on success and a error condition is passed to this callback.
+     * @param handler the handler to call on success.
+     * @param <R> type of the returned success value.
+     * @return a new callback.
+     * <h2>Example</h2>
+     * <pre>
+     *     public void sub(Callback&lt;Long&gt; cb) {
+     *         subsub(cb.call(value -&gt; {
+     *             cb.ok(value + 1);
+     *         }
+     *     }
+     *
+     *     public void main() {
+     *         sub((v, e) -&gt; {
+     *             System.out.println(v + " " + e);
+     *         }
+     *     }
+     *
+     *     public void subsub(Callback&lt;Long&gt; cb) {
+     *         cb.ok(42);
+     *     }
+     * </pre>
+     */
     default <R> Callback<R> call(Consumer<R> handler) {
         return (t, e) -> {
             if (e != null) {
@@ -40,35 +74,94 @@ public interface Callback<T> {
         };
     }
 
+    /**
+     * Return a new {@code Handler<AsyncResult<R>>} where the handler argument is invoked on success and a error condition is passed to this callback.
+     * @param handler the handler to call on success.
+     * @param <R> type of the returned success value.
+     * @return a new vertx {@code Handler<AsyncResult<R>>}.
+     *
+     * <h2>Example</h2>
+     * <pre>
+     *     public void main() {
+     *         sub((v, e) -&gt; {
+     *             System.out.println(v + " " + e);
+     *         }
+     *     }
+     *
+     *     public void sub(Callback&lt;Void&gt; cb) {
+     *         vertx.fileSystem().open("filename.txt", new OpenOptions(), cb.handler(asyncfile -&gt; {
+     *             asyncfile.close(cb.handler($ -&gt; {
+     *                 cb.ok());
+     *             }
+     *         }
+     *     }
+     * </pre>
+     */
     default <R> Handler<AsyncResult<R>> handler(Consumer<R> handler) {
         return ar -> {
             if (ar.failed()) {
                 call(null, ar.cause());
             } else {
-                handler.accept(ar.result());
+                try {
+                    handler.accept(ar.result());
+                } catch (Throwable exc) {
+                    call(null, exc);
+                }
             }
         };
     }
 
+    /**
+     * finish the callback with an success value.
+     * @param value the value to send back.
+     */
     default void ok(T value) {
         call(value, null);
     }
 
+    /**
+     * Finish the callback with a null success value.
+     */
     default void ok() {
         call(null, null);
     }
 
-
+    /**
+     * Finish the callback with a failure condition.
+     * @param error the error to send back.
+     */
     default void fail(Throwable error) {
         call(null, error);
     }
 
-
-    default <E> void forEach(List<E> list, BiConsumer<E, Callback<Void>> x) {
-        forEach(list, x, this::ok);
+    /**
+     * Iterate sequentially over the elements in the list. For each element call the elmHandler.
+     * @param <E> type of elements in the list
+     * @param list the list of data
+     * @param elmHandler the elmHandler to call for each element.
+     *
+     * The elmHandler take two arguments, the actual element and a Callback that must be used to signal success or failure
+     * of handling the element. Only when the callback's ok() method is called will the next element be iterated.
+     *
+     * Calling fail() of the callback will stop the iteration.
+     */
+    default <E> void forEach(List<E> list, BiConsumer<E, Callback<Void>> elmHandler) {
+        forEach(list, elmHandler, this::ok);
     }
 
-    default <E> void forEach(List<E> list, BiConsumer<E, Callback<Void>> x, Consumer<T> done) {
+    /**
+     * Iterate sequentially over the elements in the list. For each element call the elmHandler.
+     * @param <E> type of elements in the list
+     * @param list the list of data
+     * @param elmHandler the elmHandler to call for each element.
+     * @param done the done handler that is invoked when all elements have been iterated successfully.
+     *
+     * The elmHandler take two arguments, the actual element and a Callback that must be used to signal success or failure
+     * of handling the element. Only when the callback's ok() method is called will the next element be iterated.
+     *
+     * Calling fail() of the callback will stop the iteration and the done handler will not be called.
+     */
+    default <E> void forEach(List<E> list, BiConsumer<E, Callback<Void>> elmHandler, Consumer<T> done) {
         Callback<T> thiz = this;
 
         Callback<Void> h = new Callback<Void>() {
@@ -100,7 +193,7 @@ public interface Callback<T> {
 
                         cnt.incrementAndGet();
                         //System.out.println("forEach accept");
-                        x.accept(list.get(i), this);
+                        elmHandler.accept(list.get(i), this);
                         if (completed == null || cnt.get() > completed.get()) {
                             break;
                         }
